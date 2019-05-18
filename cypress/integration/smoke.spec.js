e66023cb0c439701357ff43fa35a38a4cfef4294
Indra Lukmana <indralukmana@live.com>
2019-05-18 21:47:31 +0700

    Setup eslint

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,7 +1,7 @@
 import { isTag } from 'postcss-selector-parser';
 
 describe('Smoke Test', () => {
-	it('Test welcom message', () => {
-		cy.visit('http://localhost:1234').contains('Hello TDD!');
-	});
+  it('Test welcom message', () => {
+    cy.visit('http://localhost:1234').contains('Hello TDD!');
+  });
 });
--- test/unit/App.spec.js
+++ test/unit/App.spec.js
@@ -3,8 +3,8 @@ import { mount } from 'enzyme';
 import App from '../../src/App';
 
 describe('App', () => {
-	it('renders hello TDD', () => {
-		const wrapper = mount(<App />);
-		expect(wrapper.find('.hello').text()).toContain('Hello TDD!');
-	});
+  it('renders hello TDD', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find('.hello').text()).toContain('Hello TDD!');
+  });
 });
