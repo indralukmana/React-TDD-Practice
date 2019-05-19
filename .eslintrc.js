7c3ae4399ee63bf3ae99c176d2a581e4f919f583
Indra Lukmana <indralukmana@live.com>
2019-05-19 14:28:27 +0700

    Use standard-react for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-	extends: ['codingitwrong'],
+	extends: ['standard', 'standard-react'],
 	parser: 'babel-eslint',
 	plugins: ['jest', 'react', 'cypress'],
 	env: {
 		browser: true,
-		'jest/globals': true,
+		'jest/globals': true
 	},
 	rules: {
-		'newline-per-chained-call': ['error', { ignoreChainWithDepth: 1 }],
-	},
-};
+		'newline-per-chained-call': ['error', { ignoreChainWithDepth: 1 }]
+	}
+}
